d2de871e787413579643f217bec54910295a56d4
OmarAlmnt <omarft8@gmail.com>
2020-03-19 14:59:04 -0400

    driver

---
+++ src/sushiban/conexion.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sushiban;
+
+import java.sql.*;
+
+public class conexion {
+    Connection con;
+    public conexion(){
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            con=DriverManager.getConnection("jdbc:mysql://localhost:3306/woodtool","root","");
+        } catch (ClassNotFoundException | SQLException e) {
+            System.err.println("Error:" +e);
+        }
+    }
+}
+
