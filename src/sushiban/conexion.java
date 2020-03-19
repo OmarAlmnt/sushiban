c306e1aede17e251e0e368c2f363c365a8180d9c
OmarAlmnt <omarft8@gmail.com>
2020-03-19 15:44:21 -0400

    probar conexion

--- src/sushiban/conexion.java
+++ src/sushiban/conexion.java
@@ -4,7 +4,6 @@
  * and open the template in the editor.
  */
 package sushiban;
-
 import java.sql.*;
 
 public class conexion {
@@ -12,7 +11,7 @@ public class conexion {
     public conexion(){
         try {
             Class.forName("com.mysql.jdbc.Driver");
-            con=DriverManager.getConnection("jdbc:mysql://sql112.epizy.com/epiz_25365054_sushiban","epiz_25365054","8ROhcEJNJwn0");
+            con=DriverManager.getConnection("jdbc:mysql://b7o3f0z8mwzsjgdgl2qw-mysql.services.clever-cloud.com:3306/b7o3f0z8mwzsjgdgl2qw?autoReconnect=true&useSSL=false","ug848wzvhsoop2qb","OXPwl3nicWsclMYDgUwV");
             System.out.println("funciona");
         } catch (ClassNotFoundException | SQLException e) {
             System.err.println("Error:" +e);
